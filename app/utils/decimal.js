6201ef27c14f03394139307e75a81fe998f1119e
Chainsulting - DLT Consulting & Development <support@chainsulting.de>
2019-08-06 20:32:18 +0200

    Add Dashboard and Loading Screen

---
+++ app/utils/decimal.js
@@ -0,0 +1,30 @@
+import { Decimal } from 'decimal.js'
+// import { DECIMAL } from '~/constants/decimal'
+
+function truncateAmount(amount: Decimal) {
+  return amount.toFixed(2);
+}
+
+function toDecimalPlaces(amount: Decimal, places: number = 4) {
+  return amount.toDP(places, Decimal.ROUND_FLOOR).toString()
+}
+
+function flattenDecimals(object) {
+  const result = {}
+
+  Object.keys(object).forEach(key => {
+    const value = object[key]
+
+    result[key] = typeof value === typeof Decimal(0)
+      ? Number(value.toString())
+      : value
+  })
+
+  return result
+}
+
+export {
+  truncateAmount,
+  toDecimalPlaces,
+  flattenDecimals
+}
