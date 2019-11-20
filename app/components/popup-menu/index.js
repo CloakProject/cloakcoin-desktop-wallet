a571649abc0732a9721a7f102c0424c744ac0381
chainsulting <support@chainsulting.de>
2019-11-20 18:34:42 +0100

    init beta release clean

--- app/components/popup-menu/index.js
+++
@@ -1,2 +0,0 @@
-export { default as PopupMenu } from './popup-menu';
-export { default as PopupMenuItem } from './popup-menu-item';
--- app/components/uniform-list/index.js
+++
@@ -1,4 +0,0 @@
-export { default as UniformList } from './UniformList'
-export { default as UniformListHeader } from './UniformListRow'
-export { default as UniformListRow } from './UniformListRow'
-export { default as UniformListColumn } from './UniformListColumn'
